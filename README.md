c581dc57440147d7e2aaf9d50f35a684726a1aca
Robert Anderson <ra@riemann.home>
2016-11-30 12:39:30 +0000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+## A Chrome extension to restore the lichess 'Print-friendly PDF' feature. ##
+
+ This extension has the following features:
+
+* Adds the 'Print-friendly PDF' option to any lichess game.
+
+Icon CC-BY-SA 3.0, CBurnett (https://en.wikipedia.org/wiki/User:Cburnett) 
+with adjustments by AntonSusi (https://commons.wikimedia.org/wiki/User:Antonsusi).
---
+++ ext/src/bg/exporttopdf.js
@@ -0,0 +1,14 @@
+var gameurl = $('meta[property="og:url"]').attr('content');
+if (gameurl !== null)
+{
+    if (gameurl.indexOf('/black') !== -1) {
+        gameurl = gameurl.substr(0, gameurl.lastIndexOf('/'));
+    }
+    var gameid = gameurl.substr(gameurl.lastIndexOf('/') + 1)
+    var embedInWebsite = $("div.pgn_options").find("div");
+    if (embedInWebsite !== null) {
+        //var exportToPdfLink = "http://lichess-export-pdf.eu-west-1.elasticbeanstalk.com/main.php?id=" + gameid;  
+        var exportToPdfLink = "https://e7tyur4sib.execute-api.eu-west-1.amazonaws.com/dev/export/" + gameid;  
+        embedInWebsite.append($('<a data-icon="x" class="text" target="_blank" rel="nofollow" href="' + exportToPdfLink + '">Print-friendly PDF</a>'));
+    }
+}
