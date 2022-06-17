a102b04cb770d06ac4ea8e3b31ab725d7c437f6e
Robert Anderson <github@nosredna.com>
2022-06-17 15:15:40 +0100

    Fixes for recent changes to lichess.

--- ext/src/bg/exporttopdf.js
+++ ext/src/bg/exporttopdf.js
@@ -5,18 +5,19 @@ if (gameurl !== null)
         gameurl = gameurl.substr(0, gameurl.lastIndexOf('/'));
     }
     var gameid = gameurl.substr(gameurl.lastIndexOf('/') + 1)
-    var pgnOptions = $("div.pgn-options");
+    var pgnOptions = $("strong:contains('PGN')");
     if (pgnOptions)
     {
-        var embedInWebsite = pgnOptions.find("div");
+        var embedInWebsite = pgnOptions.parent();
         if (embedInWebsite) {
             if (embedInWebsite.length === 0)
             {
-                throw Error("Could not find div within pgn-options.");
+                //throw Error("Could not find div within pgnOptions.");
+            } else {
+                //var exportToPdfLink = "http://lichess-export-pdf.eu-west-1.elasticbeanstalk.com/main.php?id=" + gameid;  
+                var exportToPdfLink = "https://e7tyur4sib.execute-api.eu-west-1.amazonaws.com/dev/export/" + gameid;
+                embedInWebsite.append($('<a data-icon="" class="text" target="_blank" rel="nofollow" href="' + exportToPdfLink + '">Print-friendly PDF</a>'));
             }
-            //var exportToPdfLink = "http://lichess-export-pdf.eu-west-1.elasticbeanstalk.com/main.php?id=" + gameid;  
-            var exportToPdfLink = "https://e7tyur4sib.execute-api.eu-west-1.amazonaws.com/dev/export/" + gameid;
-            embedInWebsite.append($('<a data-icon="x" class="text" target="_blank" rel="nofollow" href="' + exportToPdfLink + '">Print-friendly PDF</a>'));
         }
     }
 }
